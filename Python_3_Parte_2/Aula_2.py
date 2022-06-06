31db47f78cae09fc815013c7fdd6c756fa1a08e3
bdsp59 <brunodsp32@gmail.com>
2022-06-06 19:36:10 -0300

    Estudos

--- Python_3_Parte_2/Aula_2.py
+++ Python_3_Parte_2/Aula_2.py
@@ -9,9 +9,21 @@ def jogar_forca():
     acertou = False
 
     while(not enforcou and not acertou):
-        chute = input("Qual letra?")
+        chute = input("Qual letra? ")
+        chute = chute.strip #Essa função remove os espaços vazios que a palavra possa possuir.
+        
+        index = 0
+        #Apesar de existir a função find(), que verifica se o que está no () pertence a String, ele
+        #apenas retorna a primeira ocorrência do elemento. Como precisamos de todos, criamos esse for.
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("A letra {} está na posição {}".format(chute, index))
+            index += 1
 
     print("Fim do jogo.")
 
 if(__name__  == "__main__"):
     jogar_forca()
+
+
+#Para verificar as funções que a classe String possui: https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str
--- README.md
+++ README.md
@@ -20,3 +20,4 @@ Diferença entre código interpretado e compilado.
 
 ## Python 3 parte 2
 Explicando tipo booleano e continuando o jogo de forca.
+Manipulação de Strings.
